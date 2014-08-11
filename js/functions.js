e394d178d8a4a189cc66a043a62b57142886ff8e
cristian24 <cristian.cuspoca@correounivalle.edu.co>
2014-08-11 11:32:19 -0500

    Mejoras en el texto
    se modifico la forma del funcionamiento del evento de scroll, para que
    el cambio del color del nav se puede elegir desde el css y no desde el
    js

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,37 @@
 $(document).ready(function() {
 
+	/**
+	 * Evento de scroll suave, cuando se presiona
+	 * sobre un elemento del menú.
+	 * @param  {Event} event [Evento generado]
+	 * @return {boolean} [Evita que el evento se siga
+	 * ejecutando.]
+	 */
 	$(".ancla").click(function(event) {
 		var link = $(this);
         var anchor  = link.attr('name');
-         
-
-        /*$(window).scroll(function(){
-	    	if($(window).scrollTop() >= altura){
-	    		$( anchor ).effect( "slide", 2000 );     
-	        }else{
-	          	//$( anchor ).hide( "slide", 1000); 
-	        }         
-		}); */     
-
         $('html, body').stop().animate({
             scrollTop: jQuery(anchor).offset().top
         }, 1000);
         return false;
 	});	
 
+	/**
+	 * Evento que responde al movimiento del scroll
+	 * agregando una clase al nav.	 
+	 */
 	$(window).scroll(function(){		
-		var altura = $(".content").offset().top; 
+		var altura = $("#content").offset().top; 
     	if($(window).scrollTop() >= altura){
-    		 $("nav").css({
+    		$("nav").addClass('background-nav');
+    		 /*$("nav").css({
     		 	background: '#ccc'
-    		 });
+    		 });*/
         }else{
-          	 $("nav").css({
+        	$("nav").removeClass('background-nav');
+          	 /*$("nav").css({
     		 	background: 'none'
-    		 });
+    		 });*/
         }         
 	});
 
